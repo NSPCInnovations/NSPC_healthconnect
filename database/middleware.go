39c6c8f85cfd5af30bf11b1ab515a46346c0f423
mahammadameer <mahammadameer100@gmail.com>
2026-02-21 11:59:29 +0530

    middleware

---
+++ database/middleware.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		log.Printf("METHOD: %s | PATH: %s | DURATION: %v", r.Method, r.URL.Path, time.Since(start))
+	})
+}
+
+func RecoveryMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("PANIC RECOVERED: %v", err)
+				http.Error(w, "Internal Server Error", 500)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
+func JSONContentMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if (r.Method == "POST" || r.Method == "PUT") && r.Header.Get("Content-Type") != "application/json" {
+			http.Error(w, "Content-Type must be application/json", 415)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
