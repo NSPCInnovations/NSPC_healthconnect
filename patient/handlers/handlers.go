e3f17b2d43e4e4c3e89a660a4ab7f9611e84a2b2
chamundeswari <chamu6450@gmail.com>
2026-02-28 11:27:50 +0530

    handlers

---
+++ patient/handlers/handlers.go
@@ -0,0 +1,57 @@
+package patient_registration
+
+import (
+	"net/http"
+
+	"Healthcare_patient/config"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterPatient(c *gin.Context) {
+
+	var patient Patient
+
+	// 1. Read JSON request
+	if err := c.ShouldBindJSON(&patient); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "FAILED",
+			"message": "Invalid request body",
+		})
+		return
+	}
+	if patient.FirstName == "" || patient.LastName == "" ||
+		patient.MobileNumber == "" || patient.Age <= 0 || patient.Gender == "" {
+
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "FAILED",
+			"message": "Missing or invalid required fields",
+		})
+		return
+	}
+	var existingPatient Patient
+
+	err := config.DB.
+		Where("mobile_number = ?", patient.MobileNumber).
+		First(&existingPatient).Error
+
+	if err == nil {
+		c.JSON(http.StatusConflict, gin.H{
+			"status":  "FAILED",
+			"message": "Mobile number already exists",
+		})
+		return
+	}
+	if err := config.DB.Create(&patient).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status":  "FAILED",
+			"message": "Failed to register patient",
+		})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{
+		"status":     "SUCCESS",
+		"message":    "Patient registered successfully",
+		"patient_id": patient.PatientID,
+	})
+}
